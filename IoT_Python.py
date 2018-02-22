8d4764a6fdf8c42061b496c35f7fec62ce89bf4f
Yasser-Alsouda <35956373+Yasser-Alsouda@users.noreply.github.com>
2018-02-22 19:55:28 +0100

    vdjlkv

--- IoT_Python.py
+++ IoT_Python.py
@@ -11,7 +11,7 @@ b5 =0
 b6 =0
 b7 =0
 IO.setmode (IO.BCM)            
-IO.setup(4,IO.IN)                    
+IO.setup(4,IO.IN)                   
 IO.setup(17,IO.IN)
 IO.setup(27,IO.IN)
 IO.setup(22,IO.IN)
@@ -19,51 +19,50 @@ IO.setup(5,IO.IN)
 IO.setup(6,IO.IN)
 IO.setup(13,IO.IN)
 IO.setup(19,IO.IN)
-while 1:                                               # execute loop forever
-    if (IO.input(19) == True):
+while 1:                                              
         time.sleep(0.001)
         if (IO.input(19) == True):
-            b7=1                                       # if pin19 is high bit7 is true
+            b7=1                                      
 
     if (IO.input(13) == True):
         time.sleep(0.001)
         if (IO.input(13) == True):
-            b6=1                                      # if pin13 is high bit6 is true
+            b6=1                                     
 
     if (IO.input(6) == True):
         time.sleep(0.001)
         if (IO.input(6) == True):
-            b5=1                                      # if pin6 is high bit5 is true
+            b5=1                                     
 
     if (IO.input(5) == True):
         time.sleep(0.001)
         if (IO.input(5) == True):
-            b4=1                                     # if pin5 is high bit4 is true
+            b4=1                                     
 
     if (IO.input(22) == True):
         time.sleep(0.001)
         if (IO.input(22) == True):
-            b3=1                                     # if pin22 is high bit3 is true
+            b3=1                                     
 
     if (IO.input(27) == True):
         time.sleep(0.001)
         if (IO.input(27) == True):
-            b2=1                                    # if pin27 is high bit2 is true
+            b2=1                                    
  
   if (IO.input(17) == True):
         time.sleep(0.001)
         if (IO.input(17) == True):
-            b1=1                                    # if pin17 is high bit1 is true
+            b1=1                                    
 
     if (IO.input(4) == True):
         time.sleep(0.001)
         if (IO.input(4) == True):
-            b0=1                                    # if pin4 is high bit0 is true
+            b0=1                                   
     
     x = (1*b0)+(2*b1)
     x = x+(4*b2)+(8*b3)
     x = x+(16*b4)+(32*b5)
-    x = x+(64*b6)+(128*b7)                    # representing the bit values from LSB to MSB
-    print ('x = ')                                              # print the ADC value
-    b0=b1=b2=b3=b4=b5=b6=b7=0        # reset values
-    time.sleep(1)                                   # wait for 10ms
+    x = x+(64*b6)+(128*b7)                    
+    print ('x = ')                                              
+    b0=b1=b2=b3=b4=b5=b6=b7=0        
+    time.sleep(1)                                   
